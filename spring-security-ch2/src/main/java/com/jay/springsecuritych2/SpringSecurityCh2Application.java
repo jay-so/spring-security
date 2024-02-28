a605358e5267b7f34a22bdea681407abe10bb43c
jay-so <merry9504@naver.com>
2024-02-28 22:14:11 +0900

    Init: SpringSecurity ch2 프로젝트 추가

---
+++ spring-security-ch2/src/main/java/com/jay/springsecuritych2/SpringSecurityCh2Application.java
@@ -0,0 +1,13 @@
+package com.jay.springsecuritych2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityCh2Application.class, args);
+    }
+
+}
